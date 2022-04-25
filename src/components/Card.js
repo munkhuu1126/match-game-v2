97821d44794c8f2addc76e32eb668896e0d34977
munkhuu1126 <munkh1151@gmail.com>
2022-04-25 11:43:38 +0800

    first commit

---
+++ src/components/Card.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function Card({card, handleChoice, flipped, disabled}) {
+
+    const handleClick =()=>{
+        if(!disabled){
+            handleChoice(card)
+        }
+        
+    }
+
+    return (
+        <div className='card '>
+            <div className={flipped ? 'flipped': ''}>
+                <img src={card.src} alt='card-front' className=" front border-2  rounded-lg border-white" />
+                <img src='/img/cover.png' 
+                alt='card-back' 
+                onClick={handleClick} 
+                className=" back border-2 rounded-lg border-white"
+                />
+            </div>
+
+        </div>
+    )
+}
